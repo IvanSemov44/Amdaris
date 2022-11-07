2ba33adb2bd2133fd88e00417a6b178f9cb39b84
Иван Семов <ivan.semov.44@gmail.com>
2022-11-07 12:55:16 +0200

    Add C#Basic Code

---
+++ 03.AmdricC#Basic/EstateBasicSolution/EstateBasic/Estate.cs
@@ -0,0 +1,15 @@
+﻿using EstateBasic.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstateBasic
+{
+    public class Estate : IEstate
+    {
+        public string? Location { get; set; }
+        public int Price { get; set; }
+    }
+}
