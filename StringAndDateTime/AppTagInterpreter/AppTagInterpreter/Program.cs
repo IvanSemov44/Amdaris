f1d1624532f445ad05ad3d449541fd95ce1e5b36
Иван Семов <ivan.semov.44@gmail.com>
2022-11-08 12:03:56 +0200

    Add Assignment For C# String And DateTime

---
+++ StringAndDateTime/AppTagInterpreter/AppTagInterpreter/Program.cs
@@ -0,0 +1,37 @@
+﻿string? input = Console.ReadLine();
+
+
+input = input.Remove(0, 1);
+input = input.Remove(input.Length - 1, 1);
+
+string[] arr = input.Split("><", StringSplitOptions.RemoveEmptyEntries);
+
+int openTagCount = 0;
+int closeTabCount = 0;
+
+foreach (var item in arr)
+{
+    if (item == "app")
+    {
+        openTagCount++;
+    }
+    else if (item == "/app")
+    {
+        if (openTagCount > 0)
+        {
+            openTagCount--;
+        }
+        else
+        {
+            closeTabCount++;
+        }
+    }
+}
+
+int result = Math.Abs(openTagCount + closeTabCount);
+
+Console.WriteLine(result);
+
+//<app><app><app></app></app>
+//<app></app></app></app><app>
+//</app><app><app></app></app>
---
+++ StringAndDateTime/Assignment/Assignment/Assignment/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Globalization;
+
+
+Console.WriteLine("-------Split/Join------");
+
+string item = "1,2,3,4,5";
+string[] arr = item.Split(',', StringSplitOptions.RemoveEmptyEntries);
+
+Console.WriteLine(string.Join(" - ", arr));
+
+Console.WriteLine("-------DateTime------");
+
+DateTime date1 = DateTime.Now;
+DateTime date2 = DateTime.UtcNow;
+
+TimeSpan interval = date2 - date1;
+Console.WriteLine(interval);
+
+Console.WriteLine("-------DateTimeOffset------");
+
+DateTimeOffset dateTimeOffsetNow = DateTimeOffset.Now;
+DateTimeOffset dateTimeOffsetUtcNow = DateTimeOffset.UtcNow;
+
+TimeSpan intervalOffset = dateTimeOffsetNow - dateTimeOffsetUtcNow;
+Console.WriteLine(intervalOffset);
+
+Console.WriteLine("-------TimeZoneInfo------");
+
+TimeZoneInfo info1 = TimeZoneInfo.Local;
+DateTime info2 = TimeZoneInfo.ConvertTimeToUtc(date1);
+
+Console.WriteLine(info1);
+Console.WriteLine(info2);
+
+Console.WriteLine("-------CultureInfo------");
+
+CultureInfo cultureInfo = new CultureInfo("bg-BG");
+
+List<string> list = new List<string>();
+list.Add(cultureInfo.ToString());
+list.Add(cultureInfo.DisplayName);
+list.Add(cultureInfo.IsNeutralCulture.ToString());
+list.Add(cultureInfo.Parent.ToString());
+list.Add(cultureInfo.Parent.ToString());
+list.Add(cultureInfo.ThreeLetterISOLanguageName.ToString());
+list.Add(cultureInfo.NativeName.ToString());
+
+
+
+Console.WriteLine(string.Join("\n",list));
