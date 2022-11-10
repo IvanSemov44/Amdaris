69b134ccc1f4e5651746ba3f84f8202dc1d57725
Иван Семов <ivan.semov.44@gmail.com>
2022-11-10 11:40:27 +0200

    Add Assigment for ArrayCollectionGeneric

---
+++ 06.ArrayCollectionGeneric/ArrayCollectionGeneric/ArrayCollectionGeneric/Collection.cs
@@ -0,0 +1,71 @@
+﻿namespace ArrayCollectionGeneric
+{
+    public class Collection<T>
+    {
+
+        private T[] _array = new T[4];
+        private int _currentIndex = 0;
+
+        public Collection()
+        {
+
+        }
+
+        private void NewArray(int length)
+        {
+            T[] newArray = new T[length * 2];
+
+            for (int i = 0; i < _array.Length; i++)
+            {
+                newArray[i] = _array[i];
+            }
+
+            _array = newArray;
+        }
+
+
+        public void Add(T item)
+        {
+            if (_currentIndex == _array.Length)
+            {
+                NewArray(_array.Length);
+            }
+            _array[_currentIndex] = item;
+
+            _currentIndex++;
+        }
+
+        public void Remove(int index)
+        {
+            T[] newArray = new T[_array.Length];
+            
+            for(int i = 0; i < _array.Length; i++)
+            {
+                if(i != index)
+                {
+                    newArray[i] = _array[i];
+                }
+            }
+            _array = newArray;
+        }
+
+        public T GetItemByIndex (int index)
+        {
+            return _array[index];
+        }
+
+        public void SetItemByIndex(int index,T item)
+        {
+            _array[index] = item;
+        }
+
+        public void SwapByIndexs(int firstIndex, int secondIndex)
+        {
+            var value = _array[firstIndex];
+            Remove(firstIndex);
+            _array[firstIndex] = _array[secondIndex];
+            _array[secondIndex] = value;
+        }
+
+    }
+}
