adab2cbd4208556417fc3ee4d8dea17e9a27fed1
Иван Семов <ivan.semov.44@gmail.com>
2022-11-18 15:43:51 +0200

    SingletonDesignPatternThreadSafe

---
+++ 10.Design Patterns - Creational/Design Patterns - Creational/SingletonDesignPatternThreadSafe/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using SingletonDesignPatternThreadSafe;
+
+Parallel.Invoke(
+    ()=>PrintStudentDetails(),
+    ()=>PrintTeacherDetails());
+
+ static void PrintTeacherDetails()
+{
+    Singleton fromTeacher = Singleton.GetInstance;
+    fromTeacher.PrintDetails("From Teacher");
+}
+
+static void PrintStudentDetails()
+{
+    Singleton fromStudent = Singleton.GetInstance;
+    fromStudent.PrintDetails("From Student");
+}
---
+++ 10.Design Patterns - Creational/Design Patterns - Creational/SingletonDesignPatternThreadSafe/Singleton.cs
@@ -0,0 +1,28 @@
+﻿
+namespace SingletonDesignPatternThreadSafe
+{
+    public sealed class Singleton
+    {
+        private static int count = 0;
+
+        private Singleton()
+        {
+            count++;
+            Console.WriteLine("Count Value:" + count);
+        }
+
+        private static readonly Lazy<Singleton> InstanceLock = new Lazy<Singleton>(() => new Singleton());
+
+        public static Singleton GetInstance
+        {
+            get
+            {
+                return InstanceLock.Value;
+            }
+        }
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
