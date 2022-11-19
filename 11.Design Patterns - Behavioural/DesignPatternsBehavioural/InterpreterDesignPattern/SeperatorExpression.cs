6685065d3970d7166fefb2dd4bcfd11a0c552096
Иван Семов <ivan.semov.44@gmail.com>
2022-11-19 18:15:35 +0200

    InterpreterDesignPattern

---
+++ 11.Design Patterns - Behavioural/DesignPatternsBehavioural/InterpreterDesignPattern/SeperatorExpression.cs
@@ -0,0 +1,13 @@
+﻿
+namespace InterpreterDesignPattern
+{
+    public class SeperatorExpression : AbstractExpression
+    {
+        public void Evaluate(Context context)
+        {
+
+            string expression = context.Expression;
+            context.Expression = expression.Replace(" ", "-");
+        }
+    }
+}
---
+++ 11.Design Patterns - Behavioural/DesignPatternsBehavioural/InterpreterDesignPattern/YearExpression.cs
@@ -0,0 +1,12 @@
+﻿
+namespace InterpreterDesignPattern
+{
+    public class YearExpression : AbstractExpression
+    {
+        public void Evaluate(Context context)
+        {
+            string expression = context.Expression;
+            context.Expression = expression.Replace("YYYY", context.Date.Year.ToString());
+        }
+    }
+}
